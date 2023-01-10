a04a511c8812a8c40a05cf0edbaf1de7441da570
sdfernandez8 <115545803+sdfernandez8@users.noreply.github.com>
2023-01-10 20:22:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var arr=[];
+var namej=document.querySelector("#namej");
+var name=document.querySelector("#name");
+arr.push("jname","name","Sim","Sammy","pravish","yaakov","jagesh","Jason");
+for(var i=0;i<arr.length;i++)
+{
+	var names=arr[i];
+	var res=names.charAt(0);
+	if(res==="j"||res==="J")
+	{
+		console.log("Goodbye "+names);
+	}
+	else
+	{
+		console.log("Hello "+names);
+	}
+
+}
